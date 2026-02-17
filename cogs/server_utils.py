57b73b0277f07d6d8291d84acccf6967f563721f
Lautaro Guzzio <lauguzzio@gmail.com>
2026-02-16 21:42:51 -0300

    convert status command message response into embed

--- cogs/server_utils.py
+++ cogs/server_utils.py
@@ -22,10 +22,24 @@ class ServerUtils(commands.Cog):
                 token=GUILD_SETTINGS["token"], 
                 action="status"
             )
-            await interaction.followup.send(f"**Server Status:** {data}")
+            embed = discord.Embed(
+                title=":green_circle: Server Online",
+                description=data,
+                color=discord.Color.green()
+            )
+            embed.set_footer(text=GUILD_SETTINGS["sv_ip"])
+            embed.timestamp = discord.utils.utcnow()
+            await interaction.followup.send(embed=embed)
 
         except MinecraftNetworkError as e:
-            await interaction.followup.send(f"{e.message}", ephemeral=True)
+            embed = discord.Embed(
+                title=":red_circle: Server offline or unreachable",
+                description=e.message,
+                color=discord.Color.red()
+            )
+            embed.set_footer(text=GUILD_SETTINGS["sv_ip"])
+            embed.timestamp = discord.utils.utcnow()
+            await interaction.followup.send(embed=embed)
         except Exception as e:
             await interaction.followup.send(f"An unexpected error occurred: {e}", ephemeral=True)
 
